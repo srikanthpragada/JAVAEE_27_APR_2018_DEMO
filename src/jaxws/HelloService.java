eb91788aed91473bffed8b208d9d8acd81b59e9b
srikanthpragada <srikanthpragada@gmail.com>
2018-05-30 20:03:21 +0530

    30-MAY

---
+++ src/jaxws/HelloService.java
@@ -0,0 +1,12 @@
+/**
+ * HelloService.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.4 Apr 22, 2006 (06:55:48 PDT) WSDL2Java emitter.
+ */
+
+package jaxws;
+
+public interface HelloService extends java.rmi.Remote {
+    public java.lang.String hello() throws java.rmi.RemoteException;
+}
